5e773a47f662f7d697941d51251771802b055841
Alex <sanasol2008@yandex.ru>
2019-04-29 02:00:03 +0300

    Update cp_vfp_logs.1.sql

--- schemas/logindb/cp_vfp_logs.1.sql
+++ schemas/logindb/cp_vfp_logs.1.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `cp_vfp_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sites_id` int(11) NOT NULL,
   `timestamp_expire` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `timestamp_voted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `timestamp_voted` timestamp NULL,
   `ipaddress` varchar(155) NOT NULL,
   `account_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
